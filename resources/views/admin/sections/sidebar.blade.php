cd2785378b8b970091163b6b7c5b141e1bce93f3
poriya <poriya77armaghani@gmail.com>
2024-11-17 23:16:44 +0330

    tag curd done

--- resources/views/admin/sections/sidebar.blade.php
+++ resources/views/admin/sections/sidebar.blade.php
@@ -114,6 +114,8 @@
         </a>
         <div id="collapseProducts" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="{{ route('admin.categories.index') }}">دسته بندی ها</a>
+                <a class="collapse-item" href="{{ route('admin.tags.index') }}">تگ ها</a>
                 <a class="collapse-item" href="{{ route('admin.attributes.index') }}">ویژگی ها</a>
             </div>
         </div>
